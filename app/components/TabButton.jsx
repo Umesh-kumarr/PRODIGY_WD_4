5eb9cc9e6c7f297ea2f815031a7a05bc31c11eb0
UMESH KUMAR <UMESH_223722@SAITM.org>
2024-07-24 21:30:07 +0530

    updated

---
+++ app/components/TabButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TabButton = ({active, selectTab, children}) => {
+  return (
+    <button className="mr-3 font-semibold hover:text-white text--[#ADB7BE] border-b border-blue-500">
+       skills
+    </button>
+  )
+}
+
+export default TabButton
