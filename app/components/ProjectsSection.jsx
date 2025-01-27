b15808bb0996aef9f4e68b44770c994430ef2268
UMESH KUMAR <UMESH_223722@SAITM.org>
2025-01-27 19:56:50 +0530

    new

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,84 +1,113 @@
-import React from 'react';
-import ProjectCard from './ProjectCard';
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
-    id:1,
-    title:"snjdsnh",
-    description:"Project 1 decription",
-    image:"/images/projects/1.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 1,
+    title: "snjdsnh",
+    description: "Project 1 decription",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
-    id:2,
-    title:"snjdsnh",
-    description:"Project 2 decription",
-    image:"/images/projects/2.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 2,
+    title: "snjdsnh",
+    description: "Project 2 decription",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
-    id:3,
-    title:"snjdsnh",
-    description:"Project 3 decription",
-    image:"/images/projects/3.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 3,
+    title: "snjdsnh",
+    description: "Project 3 decription",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
-    id:4,
-    title:"snjdsnh",
-    description:"Project 4 decription",
-    image:"/images/projects/4.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 4,
+    title: "snjdsnh",
+    description: "Project 4 decription",
+    image: "/images/projects/4.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
-    id:5,
-    title:"snjdsnh",
-    description:"Project 5 decription",
-    image:"/images/projects/5.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 5,
+    title: "snjdsnh",
+    description: "Project 5 decription",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
-    id:6,
-    title:"snjdsnh",
-    description:"Project 6 decription",
-    image:"/images/projects/6.png",
-    tag:["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    id: 6,
+    title: "snjdsnh",
+    description: "Project 6 decription",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
-  return (
-   <>
-   <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
-    My Project
-   </h2>
-   <div className="grid md:grid-cols-3 gap-8 md:gap-12 ">
-    {projectsData.map((project) => 
-    <ProjectCard
-    key={project.id}
-     title ={project.title} 
-     description={project.description} 
-     imgUrl={project.image}
-     tags={project}
-     gitUrl={project.gitUrl}
-     previewUrl={project.previewUrl}
-   />
-   )}
-   </div>
-   </>
-  )
-}
+  const [tag, setTag] = useState("All");
 
-export default ProjectsSection
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12 ">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            tags={project.tag}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
